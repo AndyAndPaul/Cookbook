113f53b85b96b9257ab42173d2ffdd9c8356db1f
Paul Vasich <paul.vasich@gmail.com>
2016-01-06 14:29:03 -0600

    up-to-date

---
+++ public/js/views/add-recipe.js
@@ -0,0 +1,94 @@
+var app = app || {};
+var active = active || {};
+
+app.createRecipeView = Backbone.View.extend({
+  el: '#add-recipe-form',
+  events: {
+    'click .create-recipe-button': 'createRecipe',
+    'click .new-ingredient': 'addIngredientLine'
+  },
+  createRecipe: function() {
+    console.log('adding recipe');
+    var ingredients = [];
+    $('.add-ingredient').each(function(rawIngredient) {
+      console.log($(this));
+      var cookedIngredient = {};
+      cookedIngredient.quantity = {};
+      cookedIngredient.quantity.whole = $(this).children('.qty-whole').val();
+      cookedIngredient.quantity.numerator = $(this).children('.qty-numerator').val();
+      cookedIngredient.quantity.denominator = $(this).children('.qty-denominator').val();
+      cookedIngredient.unit = $(this).children('.unit').val();
+      cookedIngredient.ingredient = $(this).children('.ingredient-name').val();
+      ingredients.push(cookedIngredient);
+    });
+    var instructions = [];
+    $('.add-instruction').each(function(instruction) {
+      instructions.push($(this).val());
+    });
+    var tags = [];
+    $('.add-tag').each(function(tag) {
+      tags.push($(this).val());
+    });
+    var recipe = {
+      name: $('#recipe-name').val(),
+      prepTime: $('#prep-time').val(),
+      cookTime: $('#cook-time').val(),
+      ingredients: ingredients,
+      instructions: instructions,
+      tags: tags
+    };
+    console.log(recipe);
+    var cookbookId = (this.collection.url.split('cookbook/')[1]);
+    this.collection.url = '/api/recipe/' + cookbookId;
+    this.collection.create(recipe);
+    $('#add-recipe-form').hide();
+  },
+  addIngredientLine: function() {
+    console.log('clicked');
+    var html = _.template($('#add-ingredient-template').html());
+    $('#add-ingredients-container').append(html);
+  },
+  initialize: function() {
+    console.log('addRecipeView instantiated');
+    console.log(this);
+  }
+  // validateInput: function() {
+  //   var inputs = this.$el.children('input');
+  //   console.log(inputs);
+  //   var haveValues = false;
+  //   var valid = false;
+  //   for( var i = 0; i < inputs.length; i++ ) {
+  //     var input = $(inputs)[i];
+  //     value = $(input).val();
+  //     console.log(value);
+  //     if(value != null && value != undefined && value != '' ) {
+  //       valid = true;
+  //     } else {
+  //       valid = false;
+  //     }
+  //   }
+  //   if(valid) {
+  //     $('.error').hide();
+  //   } else {
+  //     $('.error').html('<p>Please fill out all fields.</p>');
+  //   }
+  // }
+});
+
+
+
+$(document).ready( function() {
+  // display add recipe form when 'add new' button is clicked
+  $('#add-recipe-button').on('click', function() {
+    if(!($( "#add-recipe-form" ).length)) { // display the form if it's not already displayed
+      var html = _.template($('#add-recipe-template').html());
+      $('#recipes-container').prepend(html);
+      // scroll to form
+      $('body, html').animate({ scrollTop: $("#add-recipe-form").offset().top -200 }, 1000);
+      // instantiate Bakcbone view for form
+      active.createRecipeView = new app.createRecipeView({
+        collection: active.collection
+      });
+    }
+  });
+});
